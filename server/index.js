02bb9969e0e378b964de9957dcbf6101055104e0
Hassaan Ali <58141711+hassaanali723@users.noreply.github.com>
2021-08-25 03:08:24 +0500

    fix node version issue

--- server/index.js
+++ server/index.js
@@ -9,3 +9,13 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://memories_node:memories_node@cluster0.yg295.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true,useUnifiedTopology: true})
+ .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+
+mongoose.set('useFindAndModify',false);
