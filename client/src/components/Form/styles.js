db0bd4b5f50727673c75ddc4c26825b817673deb
Hassaan Ali <58141711+hassaanali723@users.noreply.github.com>
2021-10-09 01:59:06 +0500

    add form

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles =  makeStyles((theme) => ({
   root: {
-    '& .MuiTextField-root': {
+    '&.MuiTextField-root': {
       margin: theme.spacing(1),
     },
   },
